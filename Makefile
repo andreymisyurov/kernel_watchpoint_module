71036dd3caaeb561a4262bb13b061ea17ce87bfc
amisyurov.c <amisyurov.c@arlo.com>
2023-09-23 19:24:35 +0600

    Addition and clean watchpoint

--- Makefile
+++ Makefile
@@ -1,7 +1,31 @@
-obj-m += module_kernel.o
+# скопировал .config
+# из /home/misyurov/poky/build/tmp/work-shared/qemux86-64/kernel-build-artifacts
+# в /home/misyurov/poky/build/tmp/work-shared/qemux86-64/kernel-source/
+# вызвал make и ушел поесть. когда пришел и скомпилировал все получилось
+
+ARCH=x86_64
+CROSS_COMPILE=x86_64-poky-linux-
+
+KERNEL_SRC=/home/misyurov/poky/build/tmp/work-shared/qemux86-64/kernel-source/
+
+MODULE_NAME=module_kernel
+MODULE_SRC=module_kernel.c
+
+CC = $(CROSS_COMPILE)gcc
+EXTRA_CFLAGS += -I/home/misyurov/poky/build/tmp/work-shared/qemux86-64/kernel-build-artifacts/include
+
+obj-m += $(MODULE_NAME).o
 
 all:
-	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+	make -C $(KERNEL_SRC) M=$(PWD) ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_COMPILE) modules
 
 clean:
-	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
+	make -C $(KERNEL_SRC) M=$(PWD) ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_COMPILE) clean
+
+install:
+	cp $(MODULE_NAME).ko /path/to/target/rootfs/$(MODULE_NAME).ko
+
+uninstall:
+	rm -f /path/to/target/rootfs/$(MODULE_NAME).ko
+
+.PHONY: all clean install uninstall
--- module_kernel.c
+++ module_kernel.c
@@ -1,20 +1,47 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <asm/debugreg.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Andrey Misyurov");
 MODULE_DESCRIPTION("My test task for the best job on the world - kernel module");
 MODULE_VERSION("0.01");
 
+static unsigned long address = 0;
+
+module_param(address, ulong, 0644);
+MODULE_PARM_DESC(address, "Memory address for the watch point");
+
+
+
+static void set_watchpoint(unsigned long addr) {
+  set_debugreg(addr, 0);
+  unsigned long dr7 = 0;
+  get_debugreg(dr7, 7);
+  dr7 |= (1 << 0) | (3 << 16);
+  set_debugreg(dr7, 7);
+}
+
+static void clear_watchpoint(void) {
+  unsigned long dr7 = 0;
+  get_debugreg(dr7, 7);
+  dr7 &= ~(1 << 0);
+  set_debugreg(dr7, 7);
+}
+
+
+
 static int __init module_kernel_init(void) {
-  printk(KERN_INFO "Debug module loaded!\n");
+  printk(KERN_INFO "Debug module loaded! address: 0x%lx\n", address);
+  set_watchpoint(address);
   return 0;
 }
 
 static void __exit module_kernel_exit(void) {
-  printk(KERN_INFO "Debug module unloaded!\n");
+  printk(KERN_INFO "Debug module unloaded! address: 0x%lx\n", address);
+  clear_watchpoint();
 }
 
-module_init(debug_module_init);
-module_exit(debug_module_exit);
+module_init(module_kernel_init);
+module_exit(module_kernel_exit);
